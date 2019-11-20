df54cdd3f475b9343b568dfff7ec52dea0d4144e
medbenhadjali <Benhadjail_mohamed@yahoo.fr>
2019-11-20 13:16:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Timer from './component/Timer';
+function App() {
+  return (
+    <div className="App">
+     <Timer/>
+    </div>
+  );
+}
+
+export default App;
